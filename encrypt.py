e22b937ff65fc722b3a2363555862a89316f230f
linsole <linshunlong.f@foxmail.com>
2022-07-27 20:50:03 +0800

    add support to specify build location

--- encrypt.py
+++ encrypt.py
@@ -5,7 +5,7 @@ from Cython.Build import cythonize
 
 class Encryptor():
     def __init__(self, work_dir=".", build_dir="build", except_path=[]):
-        # platform consistency
+        # cross platform consistency
         if platform.system() == "Windows":
             self.suffix = "pyd"
         elif platform.system() == "Linux":
@@ -13,41 +13,49 @@ class Encryptor():
         else:
             raise Exception("Platform not supported! Currently supports: Windows and Linux.")
 
+        # indicates the beginning of encrypting, for later use to clean up intermediate files
         self.start_time = time.time()
 
         self.work_dir = work_dir
         self.build_dir = build_dir
         self.except_path = [os.path.abspath(path) for path in except_path]
-        self.except_path.append(os.path.abspath(self.build_dir)) # to be discussed
+        self.except_path.append(os.path.abspath(self.build_dir))
         
         self.py_file_list = []
 
     def traverse(self, base_path=None, clean=False):
+        # defalut of base_path is working directory
         if base_path is None:
             base_path = self.work_dir
 
         for path in os.listdir(base_path):
             full_path = os.path.join(base_path, path)
 
+            # use absolute path to judge if current path is in exception path
             if os.path.abspath(full_path) in self.except_path:
                 continue
 
             elif os.path.isdir(full_path):
                 if not path.startswith("."):
-                    self.traverse(full_path, clean)
+                    self.traverse(full_path, clean) # recurssively traverse
 
             elif os.path.isfile(full_path):
                 path_name_split = path.split(".")
                 if clean and path_name_split[-1] in ("c", "o", "def") \
                     and os.stat(full_path).st_mtime > self.start_time:
-                    os.remove(full_path)
+                    os.remove(full_path) # clean up intermediate files
 
                 elif path_name_split[-1] in ("py", "pyx") and not path.startswith("__") \
                     and path != __file__.split("\\")[-1] and path != __file__.split("/")[-1]:
                     self.py_file_list.append(full_path)
             
 
-    def rename(self, base_path):
+    def rename(self, base_path=None):
+        # recurssively traverse and rename generated .pyd files
+        # (for example: rename utils.cp38-win_amd64.pyd to utils.pyd)
+        if base_path is None:
+            base_path = self.build_dir
+
         for path in os.listdir(base_path):
             full_path = os.path.join(base_path, path)
             if os.path.isdir(full_path):
@@ -70,7 +78,7 @@ class Encryptor():
         self.traverse(clean=True)
 
         # rename pyd files (or .so files)
-        self.rename(self.build_dir)
+        self.rename()
 
 
 if __name__ == "__main__":
@@ -79,10 +87,15 @@ if __name__ == "__main__":
         encrypt", required=False)
     parser.add_argument("-e", default=[], nargs='*', help="Exception directories and files \
         that you don't want to encrypt", required=False)
+    parser.add_argument("-b", default="build", help="Build directory of the encrypted \
+        files", required=False)
     args = vars(parser.parse_args())
     
-    encryptor = Encryptor(work_dir=args["w"], except_path=args["e"])
+    encryptor = Encryptor(work_dir=args["w"], except_path=args["e"], build_dir=args["b"])
     encryptor.traverse()
     print("\n".join(encryptor.py_file_list))
     print(len(encryptor.py_file_list))
-    # encryptor.encrypt()
+    
+    # uncomment below to encrypt
+    encryptor.encrypt()
+    print(f"\nENCRYPTION COMPLETE! \nThe results are in: {args['b']}")
